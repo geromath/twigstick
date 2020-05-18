cab35072ffbcaa0584ca2b45ff30f536bc2141ac
geromath <mathias.maagero@gmail.com>
2020-05-18 16:19:11 +0200

    Updates

---
+++ precache-manifest.d87838fe568f9abc6b83270aa826b8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb758402e4109234285d93bc6db714b6",
+    "url": "/twigstick/index.html"
+  },
+  {
+    "revision": "7267171341a37eb1499c",
+    "url": "/twigstick/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cae69138fcb73aa82393",
+    "url": "/twigstick/static/js/2.825d39eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twigstick/static/js/2.825d39eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7267171341a37eb1499c",
+    "url": "/twigstick/static/js/main.beeb72ac.chunk.js"
+  },
+  {
+    "revision": "bb67149f9e4cbf453f9f",
+    "url": "/twigstick/static/js/runtime-main.5dfe2939.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twigstick/static/media/logo.5d5d9eef.svg"
+  }
+]);
