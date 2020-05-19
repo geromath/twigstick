0035997d43316821209033f27c638f47b17039f2
geromath <mathias.maagero@gmail.com>
2020-05-19 09:06:39 +0200

    Adding components to their own files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/projects'>PROJECTS</Link>
+            </li>
+            <li>
+              <Link to='/about'>GAMES</Link>
+            </li>
+            <li>
+              <Link to='/twigstick'>HOME</Link>
+            </li>
+            <li>
+              <Link to='/contact'>CONTACT</Link>
+            </li>
+            <li>
+              <Link to='/about'>ABOUT</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/projects'>
+            <Projects />
+          </Route>
+          <Route path='/games'>
+            <Games />
+          </Route>
+          <Route path='/twigstick'>
+            <Home />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
+function Games() {
+  return <h2>Games</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
 }
 
 export default App;
